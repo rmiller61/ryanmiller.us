a4efe73dbf715b3e09cdfea0fc30d460b59a38f5
Ryan <ryanmiller732@gmail.com>
2023-02-16 18:10:51 -0500

    format via prettier

---
+++ frontend/.prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  semi: false,
+  printWidth: 100,
+}
