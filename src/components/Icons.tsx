04793ce86ec53045c6777f9c7d8d00e75fecba63
Ryan <ryanmiller732@gmail.com>
2024-10-21 21:11:31 -0400

    resume

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -9,9 +9,10 @@ export const ChevronDoubleRight: Icon = (props) => (
     strokeWidth="0"
     viewBox="0 0 24 24"
     aria-hidden="true"
-    height="200px"
-    width="200px"
+    height="1em"
+    width="1em"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <path
       fillRule="evenodd"
