eced8fa0b8821bd060b23d1eb6afd688257b3531
Ryan <ryanmiller732@gmail.com>
2024-10-21 21:39:46 -0400

    add beats section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ export default function Button({ children, ...props }: ButtonProps) {
   return (
     <a
       {...props}
-      className="button inline-block ease-in-out duration-500 transition-all px-8 py-3 text-white rounded-full font-bold"
+      className="button inline-flex ease-in-out duration-500 transition-all px-8 py-3 text-white rounded-full font-bold items-center gap-2.5"
     >
       {children}
     </a>
