c4f12403d6af77f5163fbfbf006530bbe4dea14c
Ryan <ryanmiller732@gmail.com>
2024-11-12 20:06:13 -0500

    format

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -1,15 +1,15 @@
 export const shuffleArray = <T extends unknown>(array: T[]): T[] => {
-  let currentIndex = array.length
-  let temporaryValue: T
-  let randomIndex: number
+  let currentIndex = array.length;
+  let temporaryValue: T;
+  let randomIndex: number;
 
   while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex -= 1
-    temporaryValue = array[currentIndex] as T
-    array[currentIndex] = array[randomIndex] as T
-    array[randomIndex] = temporaryValue
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex] as T;
+    array[currentIndex] = array[randomIndex] as T;
+    array[randomIndex] = temporaryValue;
   }
 
-  return array
-}
+  return array;
+};
--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,17 +1,24 @@
-export const isEven = (num: number) => num % 2 === 0
+export const isEven = (num: number) => num % 2 === 0;
 
 export const clamp = (number: number, min: number, max: number) =>
-  Math.min(Math.max(number, min), max)
+  Math.min(Math.max(number, min), max);
 
-export const interpolate = (number: number, input: [number, number], output: [number, number]) => {
-  return ((number - input[0]) * (output[1] - output[0])) / (input[1] - input[0]) + output[0]
-}
+export const interpolate = (
+  number: number,
+  input: [number, number],
+  output: [number, number]
+) => {
+  return (
+    ((number - input[0]) * (output[1] - output[0])) / (input[1] - input[0]) +
+    output[0]
+  );
+};
 
 export const randomNumber = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 export const wrap = (number: number, min: number, max: number) => {
-  const rangeSize = max - min
-  return ((((number - min) % rangeSize) + rangeSize) % rangeSize) + min
-}
+  const rangeSize = max - min;
+  return ((((number - min) % rangeSize) + rangeSize) % rangeSize) + min;
+};
