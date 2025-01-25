1216802b6d34ca7ea7ef7d7f1a9cba32037b7493
Ryan <ryanmiller732@gmail.com>
2025-01-25 13:33:19 -0500

    all portfolio items

--- src/constants/portfolio.ts
+++ src/constants/portfolio.ts
@@ -4,6 +4,13 @@ import rocally from "../assets/projects/rocally.jpg?url";
 import sep from "../assets/projects/sep.jpg?url";
 import socialHustle from "../assets/projects/social-hustle.jpg?url";
 import stayAwhile from "../assets/projects/stay-awhile.jpg?url";
+import tm34 from "../assets/projects/tm34.webp?url";
+import altFranchise from "../assets/projects/alt-franchise.jpg?url";
+import ana from "../assets/projects/ana.jpg?url";
+import braggnBeauty from "../assets/projects/braggn-beauty.jpg?url";
+import coretrust from "../assets/projects/coretrust.jpg?url";
+import hvf from "../assets/projects/hvf.jpg?url";
+import groceryGripps from "../assets/projects/grocery-gripps.jpg?url";
 import { type Project } from "./projects";
 
 export const PORTFOLIO: Array<Project> = [
@@ -20,6 +27,13 @@ export const PORTFOLIO: Array<Project> = [
     media: {
       type: "IMAGE",
       image: gummyTech,
+    },
+  },
+  {
+    name: "TM34",
+    media: {
+      type: "IMAGE",
+      image: tm34,
     },
   },
   {
@@ -63,6 +77,48 @@ export const PORTFOLIO: Array<Project> = [
       type: "VIDEO",
       video: "/projects/super-t.mp4",
       poster: "/projects/super-t-poster.jpg",
+    },
+  },
+  {
+    name: "Alt Franchise",
+    media: {
+      type: "IMAGE",
+      image: altFranchise,
+    },
+  },
+  {
+    name: "Ana",
+    media: {
+      type: "IMAGE",
+      image: ana,
+    },
+  },
+  {
+    name: "Braggn Beauty",
+    media: {
+      type: "IMAGE",
+      image: braggnBeauty,
+    },
+  },
+  {
+    name: "Grocery Gripps",
+    media: {
+      type: "IMAGE",
+      image: groceryGripps,
+    },
+  },
+  {
+    name: "Coretrust",
+    media: {
+      type: "IMAGE",
+      image: coretrust,
+    },
+  },
+  {
+    name: "HVF",
+    media: {
+      type: "IMAGE",
+      image: hvf,
     },
   },
 ];
