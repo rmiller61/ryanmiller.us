335b64e8ce802a082234aa7d71369493a839eb79
Ryan <ryanmiller732@gmail.com>
2025-01-29 22:18:41 -0500

    fix url interface reference

--- src/src/middleware.ts
+++ src/src/middleware.ts
@@ -3,9 +3,8 @@ import { defineMiddleware } from "astro:middleware";
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
   const url = new URL(context.request.url);
-  const subdomain = url.hostname.split(".")[0];
-  if (subdomain === "portfolio") {
-    const newUrl = new URL(`https://ryanmiller.co${context.url.pathname}`);
+  if (url.hostname.includes("portfolio.")) {
+    const newUrl = new URL(`https://ryanmiller.co${url.pathname}`);
     return context.rewrite(
       new Request(newUrl, {
         headers: {
